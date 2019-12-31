9f16cf769913951f6bd0d11d389b9c68cd7a898e
bethebikash <bhattaraibikash55@gmail.com>
2019-12-31 08:25:54 +0545

    Setup retrofit to use api.

---
+++ app/src/main/java/com/bhattaraibikash/taskmanagerapp/api/UserApi.java
@@ -0,0 +1,4 @@
+package com.bhattaraibikash.taskmanagerapp.api;
+
+public interface UserApi {
+}
---
+++ app/src/main/java/com/bhattaraibikash/taskmanagerapp/url/Url.java
@@ -0,0 +1,20 @@
+package com.bhattaraibikash.taskmanagerapp.url;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Url {
+
+//    public static final String basae_url = "http://172.100.100.5:300/"; my pc's ip
+//    public static final String basae_url = "http://10.0.2.2:3000/";
+    public static final String basae_url = "http://172.100.100.5:3000/";
+
+    public static String token="";
+
+    public static Retrofit getInstance() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(basae_url).addConverterFactory(GsonConverterFactory.create()).build();
+
+        return retrofit;
+    }
+}
